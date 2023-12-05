4c09242a6758592829c9f381acd07bfa41be2717
Dmitriy_Matresko <151873547+InosukeHashibiraa@users.noreply.github.com>
2023-12-05 09:20:39 +0300

    add description for entities (#10)

--- lib/feature/domain/entities/organizations.dart
+++ lib/feature/domain/entities/organizations.dart
@@ -1,5 +1,8 @@
 import 'package:equatable/equatable.dart';
 
+/// Организация-владелец кошелька
+/// [name] - название организации
+/// [id] - идентификатор организации 
 final class OrganizationsEntity extends Equatable {
   final String name;
   final String id;
--- lib/feature/domain/entities/wallets.dart
+++ lib/feature/domain/entities/wallets.dart
@@ -2,6 +2,14 @@ import 'package:church_accounting/feature/domain/entities/organizations.dart';
 import 'package:church_accounting/feature/domain/entities/users.dart';
 import 'package:equatable/equatable.dart';
 
+/// Кошелек.
+/// [id] - идентификатор.
+/// [name] - название кошелька.
+/// [balance] - текущий баланс.
+/// [isActive] - активность кассы.
+/// [holder] - ответственный кассы.
+/// [watchers] - наблюдатели кассы.
+/// [organization] - орагнизация-владелец.
 final class WalletsEntity extends Equatable {
   final String id;
   final String name;
