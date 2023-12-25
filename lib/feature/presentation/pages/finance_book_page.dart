606d4b6525e9f0f05c75962a836135081db7435e
Dmitriy_Matresko <matresko@inbox.ru>
2023-12-26 01:53:42 +0300

    add list of widgets

--- lib/feature/presentation/pages/finance_book_page.dart
+++ lib/feature/presentation/pages/finance_book_page.dart
@@ -1,51 +1,5 @@
 import 'package:flutter/material.dart';
 
-class FinanceBookPage extends StatefulWidget {
-  const FinanceBookPage({super.key});
-
-  @override
-  State<FinanceBookPage> createState() => _FinanceBookPageState();
-}
-
-class _FinanceBookPageState extends State<FinanceBookPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFF212630),
-      child: ListView.separated(
-        itemCount: mockData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            width: double.infinity,
-            height: 145.55,
-            padding: const EdgeInsets.all(10.0),
-            decoration: const BoxDecoration(
-              color: Color(0xFF212630),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text(
-                  '$index',
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 1,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            color: const Color(0xFF333A47),
-          );
-        },
-      ),
-    );
-  }
-}
-
 // Мок данные
 final class MockData {
   final String id;
@@ -72,65 +26,197 @@ final class MockData {
       this.organization);
 }
 
-final List<MockData> mockData = [
-  MockData(
-    'text',
-    23,
-    DateTime(23, 2),
-    14.00,
-    'Кавказская молодежь',
-    'text',
-    'text',
-    'text',
-    'text',
-    'text',
-  ),
-  MockData(
-    'text',
-    23,
-    DateTime(23, 2),
-    14.00,
-    'Кавказская молодежь',
-    'text',
-    'text',
-    'text',
-    'text',
-    'text',
-  ),
-  MockData(
-    'text',
-    23,
-    DateTime(23, 2),
-    14.00,
-    'Кавказская молодежь',
-    'text',
-    'text',
-    'text',
-    'text',
-    'text',
-  ),
-  MockData(
-    'text',
-    23,
-    DateTime(23, 2),
-    14.00,
-    'Кавказская молодежь',
-    'text',
-    'text',
-    'text',
-    'text',
-    'text',
-  ),
-  MockData(
-    'text',
-    23,
-    DateTime(23, 2),
-    14.00,
-    'Кавказская молодежь',
-    'text',
-    'text',
-    'text',
-    'text',
-    'text',
-  ),
-];
+class FinanceBookPage extends StatefulWidget {
+  const FinanceBookPage({
+    super.key,
+  });
+
+  @override
+  State<FinanceBookPage> createState() => _FinanceBookPageState();
+}
+
+class _FinanceBookPageState extends State<FinanceBookPage> {
+//Мок данные
+  final List<MockData> mockData = [
+    MockData(
+      '1',
+      23,
+      DateTime(2023, 2, 23),
+      14.00,
+      'Кошелек 1',
+      'Покупка товаров',
+      'Иван Иванов',
+      'Приход',
+      'Тип операции 1',
+      'ООО Кавказская молодежь',
+    ),
+    MockData(
+      '2',
+      24,
+      DateTime(2023, 2, 24),
+      -8.50,
+      'Кошелек 2',
+      'Оплата счета',
+      'Мария Петрова',
+      'Расход',
+      'Тип операции 2',
+      'ООО "Поставщик услуг"',
+    ),
+    MockData(
+      '2',
+      24,
+      DateTime(2023, 2, 24),
+      8.50,
+      'Кошелек 2',
+      'Оплата счета',
+      'Мария Петрова',
+      'Расход',
+      'Тип операции 2',
+      'ООО "Поставщик услуг"',
+    ),
+    MockData(
+      '2',
+      24,
+      DateTime(2023, 2, 24),
+      -8.50,
+      'Кошелек 2',
+      'Оплата счета',
+      'Мария Петрова',
+      'Расход',
+      'Тип операции 2',
+      'ООО "Поставщик услуг"',
+    ),
+    MockData(
+      '2',
+      24,
+      DateTime(2023, 2, 24),
+      8.50,
+      'Кошелек 2',
+      'Оплата счета',
+      'Мария Петрова',
+      'Расход',
+      'Тип операции 2',
+      'ООО "Поставщик услуг"',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF212630),
+      child: ListView.separated(
+        itemCount: mockData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            width: double.infinity,
+            height: 145.55,
+            padding: const EdgeInsets.all(10.0),
+            decoration: const BoxDecoration(
+              color: Color(0xFF212630),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      mockData[index].sum > 0
+                          ? Icons.arrow_downward
+                          : Icons.arrow_upward,
+                      color:
+                          mockData[index].sum > 0 ? Colors.green : Colors.red,
+                    ),
+                    Text(
+                      mockData[index].user,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: 'Gothic A1',
+                        fontWeight: FontWeight.w500,
+                        height: 0.08,
+                        letterSpacing: -0.41,
+                      ),
+                    ),
+                    Text(
+                      '${mockData[index].sum} рублей',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: 'Gothic A1',
+                        fontWeight: FontWeight.w500,
+                        height: 0.08,
+                        letterSpacing: -0.41,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      mockData[index].wallet,
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(
+                        color: Color(0xFF7D8696),
+                        fontSize: 12,
+                        fontFamily: 'Gothic A1',
+                        fontWeight: FontWeight.w400,
+                        height: 0.11,
+                        letterSpacing: -0.21,
+                      ),
+                    ),
+                    Text(
+                      mockData[index].type,
+                      style: TextStyle(
+                          color: mockData[index].sum > 0
+                              ? Colors.green
+                              : Colors.red),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Комментарий:    ${mockData[index].comment}',
+                      style: const TextStyle(
+                        color: Color(0xFF7D8696),
+                        fontSize: 12,
+                        fontFamily: 'Gothic A1',
+                        fontWeight: FontWeight.w400,
+                        height: 0.11,
+                        letterSpacing: -0.21,
+                      ),
+                    ),
+                    Text(
+                      '${mockData[index].date.day}.${mockData[index].date.month}.${mockData[index].date.year}',
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(
+                        color: Color(0xFF7D8696),
+                        fontSize: 12,
+                        fontFamily: 'Gothic A1',
+                        fontWeight: FontWeight.w400,
+                        height: 0.11,
+                        letterSpacing: -0.21,
+                      ),
+                    ),
+                  ],
+                ),
+                // Добавь другие виджеты, если необходимо
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 1,
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            color: const Color(0xFF333A47),
+          );
+        },
+      ),
+    );
+  }
+}
